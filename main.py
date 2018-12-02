ce6a6dd8aa7954024365d4987541d9707ad6af20
DoHyeon Kim <kkdh7609@ajou.ac.kr>
2018-12-02 12:17:32 +0900

    main_temp

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import models
 import tensorflow as tf
 import numpy as np
+import csv
+import random
 
-data =
+all_data = []     # for shuffling data
+data_time = []
+train_data = []
+train_label = []
+
+with open('train_set.csv', 'r') as f:
+    rd = csv.reader(f)
+    for line in rd:
+        all_data.append(line)
+
+# shuffle the data
+random.shuffle(all_data)
+
+for data in all_data:
+    data_time.append([data[0]])
+    train_data.append([float(i) for i in data[2: -1]])
+    train_label.append([float(line[-1]) - 1])
+
+# Make arrays to numpy
+train_data = np.array(train_data)
+train_label = np.array(train_label)
+
+
+print("Data setting is done!")
+
